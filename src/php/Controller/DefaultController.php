194948bd30b359db3c21f299d8aee2256c7217f8
Dmitry.Krokhin <nekufa@gmail.com>
2014-04-10 20:15:16 +0400

    added location message

--- src/php/Controller/DefaultController.php
+++ src/php/Controller/DefaultController.php
@@ -2,11 +2,20 @@
 
 namespace Controller;
 
+use Cti\Core\Web;
+
 class DefaultController
 {
     function get()
     {
         echo 'Hello world!';
+    }
+
+    function processChain(Web $web, $chain)
+    {
+        $string = 'Check you base location.<br/>No method found!<br/><br/>';
+        $string .= 'Current base url: <b>%s</b><br/>Requested url: <b>%s</b>';
+        echo sprintf($string, $web->getUrl(), $web->getUrl(implode('/', $chain)));
     }
 
 }
